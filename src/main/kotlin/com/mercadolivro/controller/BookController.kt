34f6f5caf24898d6337de8b2a8b89b0bad9f15f8
Joel Schecheleski <70170415+schecheleski@users.noreply.github.com>
2021-11-08 18:34:49 -0300

    Adicionado paginação em controller books

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -7,6 +7,9 @@ import com.mercadolivro.extension.toBookModel
 import com.mercadolivro.extension.toResponse
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -26,11 +29,15 @@ class BookController(
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    fun findAll(): List<BookResponse> = bookService.findAll().map { it.toResponse() }
+    fun findAll(@PageableDefault(page = 0, size = 10) pageable: Pageable): Page<BookResponse> {
+        return bookService.findAll(pageable).map { it.toResponse() }
+    }
 
     @GetMapping("/actives")
     @ResponseStatus(HttpStatus.OK)
-    fun findActives(): List<BookResponse> = bookService.findAtives().map { it.toResponse() }
+    fun findActives(@PageableDefault(page = 0, size = 10) pageable: Pageable): Page<BookResponse> {
+        return bookService.findAtives(pageable).map { it.toResponse() }
+    }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
