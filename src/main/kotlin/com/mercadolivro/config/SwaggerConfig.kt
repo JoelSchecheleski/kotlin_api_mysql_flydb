553521aed0daa85cd18f59895cb7f0f5c5d03037
Joel Schecheleski <70170415+schecheleski@users.noreply.github.com>
2021-11-11 16:24:20 -0300

    Adicionado cors para liberar todas as origens

--- src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -23,7 +24,10 @@ class SwaggerConfig {
             .apiInfo(
                 ApiInfoBuilder()
                     .title("Mercado Livro")
+                    .version("1.0")
+                    .contact(Contact("Joel Schecheleski", "http://upper.com.br", "joel.jsp@gmail.com"))
                     .description("Api do Mercado Livro")
+                    .license("Apache License Version 2.0")
                     .build()
             )
     }
