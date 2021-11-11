fd8c4852186ea24fbf70771723b4792dea52560c
Joel Schecheleski <70170415+schecheleski@users.noreply.github.com>
2021-11-11 18:15:29 -0300

    Ajustado ControllerAdvice

--- src/main/kotlin/com/mercadolivro/enums/Errors.kt
+++ src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -1,8 +1,13 @@
 package com.mercadolivro.enums
 
 enum class Errors(val code: String, val message: String) {
+    ML000("ML-000", "Access Denied"),
     ML001("ML-001", "Invalid Request"),
+    ML002("ML-002", "Authorization Service Exception"),
+    ML003("ML-003", "You are not allowed to do this operation"),
+
     ML101("ML-101", "Book [%s] not exists"),
     ML201("ML-201", "Customer [%s] not exists"),
     ML202("ML-202", "Cannot update book with status [%s]")
+
 }
--- src/main/kotlin/com/mercadolivro/exception/ControllerAdvice.kt
+++ src/main/kotlin/com/mercadolivro/exception/ControllerAdvice.kt
@@ -3,11 +3,16 @@ package com.mercadolivro.exception
 import com.mercadolivro.controller.response.exceptionResponse.ErrorResponse
 import com.mercadolivro.controller.response.exceptionResponse.FieldErrorResponse
 import com.mercadolivro.enums.Errors
+import org.hibernate.exception.ConstraintViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.access.AuthorizationServiceException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.context.request.WebRequest
 
 
@@ -50,20 +55,55 @@ class ControllerAdvice {
         return ResponseEntity(error, HttpStatus.UNPROCESSABLE_ENTITY)
     }
 
-//    @ExceptionHandler(ConstraintViolationException::class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ResponseBody
-//    fun onConstraintValidationException(
-//        e: ConstraintViolationException
-//    ): ValidationErrorResponse? {
-//        val error = ValidationErrorResponse()
-//        for (violation in e.getConstraintViolations()) {
-//            error.getViolations().add(
-//                Violation(violation.getPropertyPath().toString(), violation.getMessage())
-//            )
-//        }
-//        return error
-//    }
+    @ExceptionHandler(AccessDeniedException::class)
+    fun handleAccessDeniedException(ex: AccessDeniedException, request: WebRequest): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            HttpStatus.FORBIDDEN.value(),
+            Errors.ML000.message,
+            Errors.ML000.code,
+            null
+        )
+        return ResponseEntity(error, HttpStatus.FORBIDDEN)
+    }
+
+    @ExceptionHandler(AuthorizationServiceException::class)
+    fun handleUnauthorizedException(e: AuthorizationServiceException): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            HttpStatus.UNAUTHORIZED.value(),
+            Errors.ML002.message,
+            Errors.ML002.code,
+            null
+        )
+        return ResponseEntity(error, HttpStatus.UNAUTHORIZED)
+    }
+
+    @ExceptionHandler(AuthenticationException::class)
+    fun handleAuthenticationException(e: AuthenticationException): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            HttpStatus.UNAUTHORIZED.value(),
+            Errors.ML003.message,
+            Errors.ML003.code,
+            null
+        )
+        return ResponseEntity(error, HttpStatus.UNAUTHORIZED)
+    }
+
+
+    @ExceptionHandler(ConstraintViolationException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    fun onConstraintValidationException(e: ConstraintViolationException): ResponseEntity<ErrorResponse> {
+        val error = ErrorResponse(
+            HttpStatus.UNAUTHORIZED.value(),
+            Errors.ML003.message,
+            Errors.ML003.code,
+            null
+        )
+        for (violation in e.getConstraintName()) {
+            error.errors!!.map { violation }
+        }
+        return ResponseEntity(error, HttpStatus.UNAUTHORIZED)
+    }
 
 //    @ExceptionHandler(
 //        ConstraintViolationException::class,
@@ -80,17 +120,6 @@ class ControllerAdvice {
 //            listOf()
 //        )
 //        return ResponseEntity(error, HttpStatus.BAD_REQUEST)
-//    }
-
-
-//    @ExceptionHandler(AuthorizationException::class)
-//    fun unauthorizedException(e: Exception): ResponseEntity<ErrorResponse> {
-//        return generateErrorResponse(HttpStatus.FORBIDDEN, "You are not authorized to do this operation", e)
-//    }
-
-//    @ExceptionHandler(AuthenticationException::class)
-//    fun forbiddenException(e: Exception): ResponseEntity<ErrorResponse> {
-//        return generateErrorResponse(HttpStatus.UNAUTHORIZED, "You are not allowed to do this operation", e)
 //    }
 
 //    @ExceptionHandler(
@@ -123,10 +152,10 @@ class ControllerAdvice {
 //        e.printStackTrace(pw)
 //        val stackTrace = sw.toString()
 
-        // example: logging the stack trace
-        // log.debug(stackTrace)
+    // example: logging the stack trace
+    // log.debug(stackTrace)
 
-        // environment-based logic
+    // environment-based logic
 //        val stackTraceMessage: String? =
 //            when (System.getenv("ENV").toUpperCase()) {
 //                "STAGING" -> stackTrace // returning the stack trace
