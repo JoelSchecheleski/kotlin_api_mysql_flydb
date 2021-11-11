8702bdfddd62409fb4339c993fca727e0c0863a3
Joel Schecheleski <70170415+schecheleski@users.noreply.github.com>
2021-11-11 15:40:39 -0300

    Ajustado para que os customers acessem somente seus proprios recursos

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -5,8 +5,11 @@ import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.controller.response.CustomerResponse
 import com.mercadolivro.extension.toCustomerModel
 import com.mercadolivro.extension.toResponse
+import com.mercadolivro.security.OnliAdminCanAccessResource
+import com.mercadolivro.security.UserCanOnlyAccessTheirOwnResource
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -23,16 +26,19 @@ class CustomerController(
     }
 
     @GetMapping
+    @OnliAdminCanAccessResource
     fun getAll(@RequestParam name: String?, @RequestParam email: String?): List<CustomerResponse> {
         return customerService.getAll(name, email).map { it.toResponse() }
     }
 
     @GetMapping("/{id}")
+    @UserCanOnlyAccessTheirOwnResource
     fun getCustomer(@PathVariable id: Int): CustomerResponse = customerService.findById(id).toResponse()
 
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @UserCanOnlyAccessTheirOwnResource
     fun update(@PathVariable id: Int, @RequestBody @Valid customer: PutCustomerRequest) {
         val customerSaved = customerService.findById(id)
         customerService.update(customer.toCustomerModel(customerSaved))
