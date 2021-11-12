545fbdba48a52343e469d8b41806d7f840f35f91
Joel Schecheleski <70170415+schecheleski@users.noreply.github.com>
2021-11-12 13:22:27 -0300

    Adicionado profilÃe de ambiente para conrole de produção e dev

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -3,7 +3,9 @@ package com.mercadolivro.controller
 import com.mercadolivro.controller.request.PostBookRequest
 import com.mercadolivro.controller.request.PutBookRequest
 import com.mercadolivro.controller.response.BookResponse
+import com.mercadolivro.controller.response.PageResponse
 import com.mercadolivro.extension.toBookModel
+import com.mercadolivro.extension.toPageResponse
 import com.mercadolivro.extension.toResponse
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
@@ -11,15 +13,14 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("books")
 class BookController(
-    val customerService: CustomerService,
-    val bookService: BookService
+    private val customerService: CustomerService,
+    private val bookService: BookService
 ) {
 
     @PostMapping
@@ -31,8 +32,8 @@ class BookController(
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    fun findAll(@PageableDefault(page = 0, size = 10) pageable: Pageable): Page<BookResponse> {
-        return bookService.findAll(pageable).map { it.toResponse() }
+    fun findAll(@PageableDefault(page = 0, size = 10) pageable: Pageable): PageResponse<BookResponse> {
+        return bookService.findAll(pageable).map { it.toResponse() }.toPageResponse()
     }
 
     @GetMapping("/actives")
--- src/main/resources/db/migration/V5__add_password_at_customer.sql
+++ src/main/resources/db/migration/V5__add_password_at_customer.sql
@@ -1,1 +1,4 @@
 alter table customers add column password varchar(255);
+
+# Atualiza os passwords
+update customers set password = '$2a$10$lm6lPuZ.geyN2IBhs1aCBeIusMy1VY80To.KGFRT4VOYD31njYFTi';
